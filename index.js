32c67175baebed17ce5a0a8b6ad3ce57be851257
Gitisfun <christianvandenputtelaar@gmail.com>
2023-05-13 22:23:04 +0200

    Add basic route

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("It works");
+import "dotenv/config";
+import Express from "express";
+import http from "http";
+import cors from "cors";
+
+// Routes
+
+const app = Express();
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("Welcome to the API!!! (v1.0)");
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
